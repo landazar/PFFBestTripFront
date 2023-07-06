d9f8bf7f993f2ffe279505f855cf270c85f81718
Emdgn <degeneve@kth.se>
2023-07-06 15:44:38 +0200

    model service experiences

--- src/app/Model/experiences.ts
+++ src/app/Model/experiences.ts
@@ -1,2 +1,16 @@
 export class Experiences {
+
+    idExperience:number;
+    nom:string;
+    dateDebut:Date;
+    dateFin:Date;
+    type:string;
+
+    constructor(idExperience:number, nom:string, dateDebut:Date, dateFin:Date, type:string) {
+        this.idExperience = idExperience;
+        this.nom = nom;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.type = type;
+    }
 }
--- src/app/Service/experiences.service.ts
+++ src/app/Service/experiences.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Experiences } from '../Model/experiences';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExperiencesService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  saveExperiences(experiences:Experiences):Observable<Experiences>
+  {
+    return this.http.post<Experiences>("http://localhost:8080/saveExperiences", experiences);
+  }
+
+  listeExperiences():Observable<Experiences[]> {
+    return this.http.get<Experiences[]>("http://localhost:8080/listeExperiences");
+  }
+
+  updateExperiencest(experiences:Experiences):Observable<boolean> {
+    return this.http.put<boolean>("http://localhost:4200/updateExperiences", experiences);
+  }
+
+  deleteExperiences(idExperience:number):Observable<boolean>
+  {
+    return this.http.delete<boolean>("http://localhost:8080/deleteExperiences/" + idExperience);
+  }
+
+  
 }
