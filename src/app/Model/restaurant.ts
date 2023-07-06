a0180f24323921a463a7bef5ce032d30b3542d9e
Gurvaan <gurvan4444@gmail.com>
2023-07-06 16:07:07 +0200

    ajout des classes activite, lieu et restaurant

---
+++ src/app/Model/restaurant.ts
@@ -0,0 +1,9 @@
+export class Restaurant {
+    type: string;
+	adresse: string;
+
+    constructor(type: string, adresse: string) {
+        this.type = type;
+        this.adresse = adresse;
+    }
+}
