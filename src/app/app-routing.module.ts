5e7bc5741a611750e9b6d12e08eb4edd37a70611
sarahborkan <sarah.behaz1@gmail.com>
2023-07-07 11:14:41 +0200

    modif

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { AjoutExperiencesComponent } from './experiencesComponent/ajout-experien
 import { ListeExperiencesComponent } from './experiencesComponent/liste-experiences/liste-experiences.component';
 import { AfficherGuideVoyageComponent } from './guide-voyage/afficher-guide-voyage/afficher-guide-voyage.component';
 import { UpdateExperiencesComponent } from './experiencesComponent/update-experiences/update-experiences.component';
+import { AjoutGuideVoyageComponent } from './guide-voyage/ajout-guide-voyage/ajout-guide-voyage.component';
 
 const routes: Routes = [
   {path: "saveExperiences", component: AjoutExperiencesComponent},
   {path: "listeExperiences", component: ListeExperiencesComponent},
   { path: 'afficher-guide-voyage', component: AfficherGuideVoyageComponent },
+  { path: 'ajouter-guide-voyage', component: AjoutGuideVoyageComponent },
 
   {path: "updateExperiences/:idExperience", component: UpdateExperiencesComponent}
 ];
