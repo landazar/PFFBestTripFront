90060450525fbd9567d21ec62601084cf32fbd06
sarahborkan <sarah.behaz1@gmail.com>
2023-07-07 09:59:12 +0200

    ajout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,16 +10,18 @@ import { ListeExperiencesComponent } from './experiencesComponent/liste-experien
 import { GuideVoyageComponent } from './guide-voyage/guide-voyage.component';
 import { AfficherGuideVoyageComponent } from './guide-voyage/afficher-guide-voyage/afficher-guide-voyage.component';
 import { AjoutExperiencesComponent } from './experiencesComponent/ajout-experiences/ajout-experiences.component';
+import { AjoutGuideVoyageComponent } from './guide-voyage/ajout-guide-voyage/ajout-guide-voyage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
 
     GuideVoyageComponent,
-    AfficherGuideVoyageComponent
+    AfficherGuideVoyageComponent,
     AjoutExperiencesComponent,
     ListeExperiencesComponent,
-    GuideVoyageComponent
+    GuideVoyageComponent,
+    AjoutGuideVoyageComponent
   ],
   imports: [
     BrowserModule,
