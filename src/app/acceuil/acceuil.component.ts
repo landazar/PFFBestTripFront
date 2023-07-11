d52894bd3d29bc12b411ff8b2b8af78d4ff11c13
Emdgn <degeneve@kth.se>
2023-07-11 12:14:39 +0200

    acceuil

--- src/app/acceuil/acceuil.component.ts
+++ src/app/acceuil/acceuil.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-acceuil',
   templateUrl: './acceuil.component.html',
   styleUrls: ['./acceuil.component.css']
 })
-export class AcceuilComponent {
+export class AcceuilComponent implements OnInit {
+
+  rechercheBarreForm!:FormGroup
+
+  constructor(private formBuilder:FormBuilder) {}
+
+  ngOnInit(): void {
+    
+    this.rechercheBarreForm = this.formBuilder.group (
+      {
+        destination:[null],
+        depart:[null],
+        type:[null]
+      }
+    );
+  }
+
+  recherche() {
+    
+  }
 
 }
