9aa78f4c9faba2f49f50ea51e135202902f13f35
Titou82Bi <inacio.baptiste@gmail.com>
2023-07-17 14:58:14 +0200

    Front changement h1 auto

--- src/app/components/header-pages/header-pages.component.ts
+++ src/app/components/header-pages/header-pages.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header-pages',
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./header-pages.component.css']
 })
 export class HeaderPagesComponent {
-
+  @Input() pageTitle!: string;
   
 }
